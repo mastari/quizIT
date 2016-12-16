a1b2e0eae36adbb04c0ea24d419ac40e3aa338e3
Ari <chocolatepie1000@gmail.com>
2016-12-16 06:46:42 -0600

    Big update to asthetic

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 $( document ).ready(function() {
     var centerH = $(document).height()/3;
+    var centerW = $(document).width()/2;
     console.log(centerH + " is the middle brake point");
     $('.center').css({
     "margin-top": centerH
+    });
+    $('.items').css({
+        "margin-right": centerW - (centerW/50)
+    });
+    $('.items').css({
+        "margin-left": centerW - (centerW/5)
+    });
+    $('.items').css({
+        "margin-top": centerH - centerH/2
     });
 });
